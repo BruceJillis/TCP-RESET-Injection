6c6269d0c419e4b2bcb97083df6b395051a31b4b
Jillis ter Hove <j.terhove@gmail.com>
2012-03-08 23:39:15 +0100

    removed forgotten recv from scapyclient.py

--- scapyclient.py
+++ scapyclient.py
@@ -9,11 +9,11 @@ sport = 59152
 dst = '127.0.0.1'
 dport = 5000
 
+# tcp 3 way handshake
 ip=IP(src=src, dst=dst)
 TCP_SYN=TCP(sport=sport, dport=dport, flags="S", seq=100)
 SYN = ip/TCP_SYN
 print SYN.show2()
-exit()
 
 TCP_SYNACK = sr1(SYN)
 print TCP_SYNACK.summary()
@@ -22,5 +22,6 @@ print TCP_SYNACK[TCP].flags
 TCP_ACK=TCP(sport=sport, dport=dport, flags="A", seq=TCP_SYNACK.ack, ack=TCP_SYNACK.seq + 1)
 send(ip/TCP_ACK)
 
+# receive first pong
 p = s.recv(512)
 print 'recv:', p.show()
